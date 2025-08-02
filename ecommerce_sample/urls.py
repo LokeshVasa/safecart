478bd21e7f3f3daef217882d8c302fa7d8843ef3
LokeshVasa <lokeshchandra842@gmail.com>
2025-08-02 16:29:18 +0530

    added_auth

--- ecommerce_sample/urls.py
+++ ecommerce_sample/urls.py
@@ -1,47 +1,32 @@
-"""
-URL configuration for ecommerce_sample project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path,include
-from store.views import product, cart, clear_data,login_user, profile, signup, wishlist, home,yourorders
+from django.urls import path, include
+from store.views import product, cart, clear_data, login_user, profile, signup, wishlist, home, yourorders
+from django.contrib import messages
+from store import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('django.contrib.auth.urls')),
-    path("", home ),
-    path("home", home, name='home' ),
-    path('cart', cart), 
-    #path('men', men), 
-    #path('women', women), 
-    #path('kids', kids), 
-    path('clear_data', clear_data), 
-    #path('electronics',electronics ), 
-    #path('furniture', furniture), 
-    # path('login/', login_user), 
-    path('profile', profile), 
-    path('signup', signup), 
-    #path('sportswear', sportswear), 
-    path('wishlist', wishlist), 
-    path('yourorders', yourorders), 
-    path('product',product),
+    path('register/', views.RegisterView, name='register'),
+    path('login/', views.LoginView, name='login'),
+    path('logout/', views.LogoutView, name='logout'),
+    path('forgot-password/', views.ForgotPassword, name='forgot-password'),
+    path('password-reset-sent/<str:reset_id>/', views.PasswordResetSent, name='password-reset-sent'),
+    path('reset-password/<str:reset_id>/', views.ResetPassword, name='reset-password'),
+    # path('', include('django.contrib.auth.urls')),  # includes login, logout, password reset, etc.
+    # Custom views
+    path("", home),
+    path("home", home, name='home'),
+    path("cart", cart),
+    path("clear_data", clear_data),
+    path("profile", profile),
+    path("wishlist", wishlist),
+    path("yourorders", yourorders),
+    path("product", product),
 ]
 
+# Serve media in development
 from django.conf import settings
 from django.conf.urls.static import static
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
